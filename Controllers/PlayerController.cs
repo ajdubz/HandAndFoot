847ce4a259076dcd17b00bd0c2bfda583c6cdfc9
ajdubz <wildauer07@gmail.com>
2024-08-08 17:14:10 -0400

    Added new db for player, team and game. Added interfaces for each, and basic services. Created player controller for adding and showing all or one player.

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using HandFootLib.Services.Interfaces;
+using HandFootLib.Models;
+using Microsoft.AspNetCore.Mvc;
+using HandFootLib.Models.DTOs;
+
+namespace HandAndFoot.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        IPlayerService _playerService;
+        public PlayerController(IPlayerService playerService) { _playerService = playerService; }
+
+
+        [HttpGet]
+        public IActionResult GetPlayers()
+        {
+            var oList = _playerService.GetPlayers();
+
+            return Ok(oList);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPlayer(int id)
+        {
+            var oPlayer = _playerService.GetPlayer(id);
+
+            return Ok(oPlayer);
+        }
+
+        [HttpPost]
+        public IActionResult AddPlayer(PlayerDTO playerDTO)
+        {
+            var oPlayer = new Player();
+
+            oPlayer.Name = playerDTO.Name;
+            oPlayer.NickName = playerDTO.NickName;
+
+            oPlayer = _playerService.AddPlayer(oPlayer);
+
+            return Ok(oPlayer);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using HandFootLib.Models;
+using HandFootLib.Services;
+using HandFootLib.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+string constr = "database=HandAndFoot;server=adubzpc; Trusted_Connection=true;TrustServerCertificate=true;";
+
+
+builder.Services.AddDbContext<Data>(op =>
+{
+    op.UseSqlServer(constr, op =>
+    {
+
+    });
+});
+
+// Add services to the container.
+builder.Services.AddScoped<IPlayerService, PlayerService>();
+builder.Services.AddScoped<ITeamService, TeamService>();
+builder.Services.AddScoped<IGameService, GameService>();
+
+
+builder.Services.AddCors(op =>
+{
+    op.AddPolicy("corsettings", policy =>
+    {
+        policy.WithOrigins("*")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -19,6 +53,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("corsettings");
 
 app.MapControllers();
 
