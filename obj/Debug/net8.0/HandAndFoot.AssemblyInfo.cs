9b98e7170682b804ecba221d5ca422c8ac8e35bc
ajdubz <wildauer07@gmail.com>
2024-08-08 13:05:37 -0400

    Added library

--- obj/Debug/net8.0/HandAndFoot.AssemblyInfo.cs
+++ obj/Debug/net8.0/HandAndFoot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandAndFoot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fbf952fc51bb6b2a0405b206040e6c0660424b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandAndFoot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandAndFoot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
